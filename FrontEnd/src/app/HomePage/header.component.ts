df979a6dce8a7ad1133d9dd5048d04cc19350237
Ahmed Adel <ahmedadelzakaria@gmail.com>
2017-12-20 11:28:43 +0200

    Added + button for add Items & add Stores

--- FrontEnd/src/app/HomePage/header.component.ts
+++ FrontEnd/src/app/HomePage/header.component.ts
@@ -8,9 +8,24 @@ import { HttpService } from '../NetworkBackEnd/http.service';
 })
 export class HeaderComponent implements OnInit {
   userName:any;
+  userType:any;
   constructor(private httpService:HttpService) { }
 
   ngOnInit() {
+    console.log(localStorage.getItem("storeID"));
+    if(localStorage.getItem("storeID")=="0")
+    {
+      this.userType="normal";
+    }
+    else if(localStorage.getItem("storeID")=="-1")
+    {
+      this.userType="superAdmin";
+    }
+    else
+    {
+      this.userType="storeAdmin";
+    }
+    console.log(this.userType)
     this.httpService.getUserData().subscribe((Response)=>{
       var Result=Response.json();
       if(Result['success']==true)
